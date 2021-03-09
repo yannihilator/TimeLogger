06c4cea6b59712957f1cd23bc17b679ff6d88eba
yannihilator <64179787+yannihilator@users.noreply.github.com>
2021-03-08 21:29:17 -0500

    adds charge number data model class

--- script.js
+++ script.js
@@ -119,3 +119,11 @@ class Entry {
         this.chargeNumber = chargeNumber;
     }
 }
+
+class ChargeNumber {
+    constructor(id, description, value) {
+        this.id = id,
+        this.description = description,
+        this.value = value
+    }
+}
