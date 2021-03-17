305d3b29e427e71f926761d4fa4c3b2d847f0508
yannihilator <64179787+yannihilator@users.noreply.github.com>
2021-03-16 22:29:52 -0400

    fixes bug where today tables don't populate when an entry does not have a charge number. continued tweaks on History UI

--- script.js
+++ script.js
@@ -262,13 +262,13 @@ function RefreshTodayUI() {
     grouped.forEach(chargeId => {
         let chargeDuration = ConvertDateTicks(chargeId.map(function(entry) { return entry.endTime - entry.startTime; }).reduce((a, b) => a + b, 0), true);
         let charge = ChargeNumberById(chargeId[0].chargeNumber);
-        let chargeNumberString = charge === undefined ? "Not found" : charge.value;
+        let chargeNumberString = charge === null ? "None" : charge.value;
         document.getElementById("chargeTotalsRows").innerHTML += '<tr>' +
           '<td>' + chargeNumberString +'</td>' +
           '<td>' + chargeDuration.hour + ":" + chargeDuration.minute + ":" + chargeDuration.second +'</td>' +
           '<td>' + (parseInt(chargeDuration.hour) + parseInt(chargeDuration.minute)/60).toFixed(1) + '</td>' +
           '<td>' + 
-            '<button id="copy_' + charge.id + '" onclick="CopyDescriptionToClipboard(' + charge.id + ')" style="background-color:#892cdc;" type="button" class="btn btn-dark btn-sm">' +
+            '<button id="copy_' + charge?.id + '" onclick="CopyDescriptionToClipboard(' + charge?.id + ')" style="background-color:#892cdc;" type="button" class="btn btn-dark btn-sm">' +
                 '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">' +
                     '<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>' +
                     '<path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>' +
@@ -284,7 +284,7 @@ function RefreshTodayUI() {
         let startTime = ConvertDateTicks(entry.startTime, false);
         let endTime = ConvertDateTicks(entry.endTime, false);
         let charge = ChargeNumberById(entry.chargeNumber);
-        let chargeNumberString = charge === undefined ? "Not found" : charge.value;
+        let chargeNumberString = charge === null ? "None" : charge.value;
         document.getElementById("entryListRows").innerHTML += '<tr>' +
           '<td>' + entry.description +'</td>' +  
           '<td>' + chargeNumberString +'</td>' +
@@ -330,7 +330,7 @@ function RefreshHistoryUI(group) {
     }
 
     //creates dropdown for each group
-    
+
 }
 
 function RefreshChargeNumberUI() {
